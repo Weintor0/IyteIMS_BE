87db6b591955fb3feda08ce65bc90e3a8624e45e
Doğu Kocatepe <kocateped@proton.me>
2024-05-17 20:31:01 +0000

    Update file import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,13 +1,17 @@
 -- Internship Coordinator
-INSERT INTO users (id, email, password, user_role)
+INSERT INTO users (id, email, password, user_role, created_date, modified_date)
 VALUES ('00000000-0000-0000-0000-000000000000',
         'internshipcoordinator@iyte.edu.tr',
         '$2a$10$32BqbxYnaFY7R9yBcO/pOO6oDoI26r/4mztyjlGfZb3eNnBtuqSpi',
-        'InternshipCoordinator'); -- 123456
+        'InternshipCoordinator',
+        now(),
+        now()); -- 123456
 
 -- Department Secretary
-INSERT INTO users (id, email, password, user_role)
+INSERT INTO users (id, email, password, user_role, created_date, modified_date)
 VALUES ('00000000-0000-0000-0000-000000000001',
         'departmentsecretary@iyte.edu.tr',
         '$2a$10$32BqbxYnaFY7R9yBcO/pOO6oDoI26r/4mztyjlGfZb3eNnBtuqSpi',
-        'DepartmentSecretary'); -- 123456
+        'DepartmentSecretary',
+        now(),
+        now()); -- 123456
