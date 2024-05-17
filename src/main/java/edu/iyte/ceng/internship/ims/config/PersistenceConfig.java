8d2701c6069d68ddbc5783a1800ef19578946ef8
Doğu Kocatepe <kocateped@proton.me>
2024-05-17 20:40:09 +0000

    Update file PersistenceConfig.java

--- src/main/java/edu/iyte/ceng/internship/ims/config/PersistenceConfig.java
+++ src/main/java/edu/iyte/ceng/internship/ims/config/PersistenceConfig.java
@@ -1,4 +1,4 @@
-package edu.iyte.ceng.internship.ims.entity;
+package edu.iyte.ceng.internship.ims.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
