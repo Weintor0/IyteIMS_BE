2ea53db04cf232a37ef047f9a133c7dbbb608609
dogu <dogukocatepe@std.iyte.edu.tr>
2024-05-17 15:23:55 +0300

    Fixed the issue related to import_files_sql_extractor and import.sql. The problem was due to Spring (and also Hibernate) version upgrade.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+-- Internship Coordinator
+INSERT INTO users (id, email, password)
+VALUES ('00000000-0000-0000-0000-000000000000',
+        'internshipcoordinator@iyte.edu.tr',
+        '$2a$10$32BqbxYnaFY7R9yBcO/pOO6oDoI26r/4mztyjlGfZb3eNnBtuqSpi'); -- 123456
+
+-- Department Secretary
+INSERT INTO users (id, email, password)
+VALUES ('00000000-0000-0000-0000-000000000001',
+        'departmentsecretary@iyte.edu.tr',
+        '$2a$10$32BqbxYnaFY7R9yBcO/pOO6oDoI26r/4mztyjlGfZb3eNnBtuqSpi'); -- 123456
