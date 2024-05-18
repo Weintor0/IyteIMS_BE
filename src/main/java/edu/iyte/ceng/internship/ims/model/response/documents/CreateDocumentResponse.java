1cb64226387d37ea58b5bae225c0cb2667e62fff
dogu <dogukocatepe@std.iyte.edu.tr>
2024-05-18 19:53:34 +0300

    Added Document upload/update/download/delete functionality.

---
+++ src/main/java/edu/iyte/ceng/internship/ims/model/response/documents/CreateDocumentResponse.java
@@ -0,0 +1,10 @@
+package edu.iyte.ceng.internship.ims.model.response.documents;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class CreateDocumentResponse {
+    private String documentId;
+}
