f23679a36de89e87591c9968d8165e0d7bca1aea
dogu <dogukocatepe@std.iyte.edu.tr>
2024-05-19 16:57:04 +0300

    Functionality needed for the initiation of internship process on the system.

---
+++ src/main/java/edu/iyte/ceng/internship/ims/controller/InternshipController.java
@@ -0,0 +1,40 @@
+package edu.iyte.ceng.internship.ims.controller;
+
+import edu.iyte.ceng.internship.ims.entity.Internship;
+import edu.iyte.ceng.internship.ims.model.response.internship.SendApplicationLetterResponse;
+import edu.iyte.ceng.internship.ims.service.InternshipService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/internship")
+public class InternshipController {
+    private final InternshipService internshipService;
+
+    @GetMapping("/get/{internshipId}")
+    public ResponseEntity<Internship> getInternshipById(String id) {
+        return new ResponseEntity<>(internshipService.getInternshipById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/get-all")
+    public ResponseEntity<List<Internship>> getInternship() {
+        return new ResponseEntity<>(internshipService.getInternships(), HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/application-letter/send/{offerId}",
+                 consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<SendApplicationLetterResponse> sendApplicationLetter(
+            @PathVariable("offerId") String offerId,
+            @RequestPart("file") MultipartFile file) throws IOException {
+        SendApplicationLetterResponse response = internshipService.sendApplicationLetter(offerId, file);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/edu/iyte/ceng/internship/ims/service/InternshipService.java
@@ -0,0 +1,158 @@
+package edu.iyte.ceng.internship.ims.service;
+
+import edu.iyte.ceng.internship.ims.entity.*;
+import edu.iyte.ceng.internship.ims.exception.BusinessException;
+import edu.iyte.ceng.internship.ims.exception.ErrorCode;
+import edu.iyte.ceng.internship.ims.model.request.notification.CreateNotificationRequest;
+import edu.iyte.ceng.internship.ims.model.response.internship.SendApplicationLetterResponse;
+import edu.iyte.ceng.internship.ims.repository.FirmRepository;
+import edu.iyte.ceng.internship.ims.repository.InternshipOfferRepository;
+import edu.iyte.ceng.internship.ims.repository.InternshipRepository;
+import edu.iyte.ceng.internship.ims.repository.StudentRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class InternshipService {
+    private final InternshipRepository internshipRepository;
+    private final NotificationService notificationService;
+    private final DocumentService documentService;
+    private final AuthenticationService authenticationService;
+
+    private final StudentRepository studentRepository;
+    private final FirmRepository firmRepository;
+    private final InternshipOfferRepository internshipOfferRepository;
+
+    public Internship getInternshipById(String id) {
+        Internship internship = internshipRepository.findById(id).orElseThrow(
+                () -> new BusinessException(ErrorCode.ResourceMissing, "Internship with id " + id + " does not exist.")
+        );
+
+        User user = authenticationService.getCurrentUser();
+        switch (user.getUserRole()) {
+            case Student: {
+                Student student = studentRepository.findStudentByUser(user).orElseThrow(
+                        () -> new IllegalStateException("UserRole implies a Student record, but it does not exist.")
+                );
+                if (!student.getId().equals(internship.getStudent().getId())) {
+                    throw new BusinessException(ErrorCode.Forbidden,
+                            "A student cannot access another student's internship information.");
+                }
+                break;
+            }
+            case Firm: {
+                Firm firm = firmRepository.findFirmByUser(user).orElseThrow(
+                        () -> new IllegalStateException("UserRole implies a Firm record, but it does not exist.")
+                );
+                if (!firm.getId().equals(internship.getInternshipOffer().getFirmId())) {
+                    throw new BusinessException(ErrorCode.Forbidden,
+                            "A firm cannot access an internship record of another company.");
+                }
+                break;
+            }
+            case DepartmentSecretary: {
+                TransactionsState state = internship.getCurrentTransactionsState();
+                if (state != TransactionsState.DepartmentSecretary &&
+                    state != TransactionsState.DeansOffice &&
+                    state != TransactionsState.Completed) {
+                    throw new BusinessException(ErrorCode.Unauthorized,
+                            "The department secretary is only responsible with internships requiring insurance");
+                }
+                break;
+            }
+            case InternshipCoordinator:
+                break;
+        }
+
+        return internship;
+    }
+
+    public List<Internship> getInternships() {
+        User user = authenticationService.getCurrentUser();
+        switch (user.getUserRole()) {
+            case Student: {
+                Student student = studentRepository.findStudentByUser(user).orElseThrow(
+                        () -> new IllegalStateException("UserRole implies a Student record, but it does not exist.")
+                );
+                return internshipRepository.findByStudent(student);
+            }
+
+            case Firm: {
+                Firm firm = firmRepository.findFirmByUser(user).orElseThrow(
+                        () -> new IllegalStateException("UserRole implies a Firm record, but it does not exist.")
+                );
+                return internshipRepository.findByInternshipOffer_FirmId(firm.getId());
+            }
+
+            case InternshipCoordinator: {
+                return internshipRepository.findAll();
+            }
+
+            case DepartmentSecretary: {
+                List<Internship> internships = new ArrayList<>();
+
+                internships.addAll(internshipRepository.findByCurrentTransactionsState(
+                        TransactionsState.DeansOffice));
+                internships.addAll(internshipRepository.findByCurrentTransactionsState(
+                        TransactionsState.DepartmentSecretary));
+                internships.addAll(internshipRepository.findByCurrentTransactionsState(
+                        TransactionsState.Completed));
+
+                return internships;
+            }
+
+            default:
+                throw new IllegalStateException("Unknown user attempted to request an internship record.");
+        }
+    }
+
+    public SendApplicationLetterResponse sendApplicationLetter(String offerId, MultipartFile file) throws IOException {
+        User user = authenticationService.getCurrentUser();
+
+        // Ensure that the user is a student
+        if (user.getUserRole() != UserRole.Student) {
+            throw new BusinessException(ErrorCode.Forbidden, "Only students can send application letters");
+        }
+
+        // Get the Student record.
+        Student student = studentRepository.findStudentByUser(user).orElseThrow(
+                () -> new IllegalStateException("UserRole implies the existence of a Student, but it does not exist.")
+        );
+
+        // Get the InternshipOffer record.
+        InternshipOffer internshipOffer = internshipOfferRepository.findInternshipOfferById(offerId).orElseThrow(
+                () -> new BusinessException(ErrorCode.ResourceMissing,
+                        "Internship offer with ID " + offerId + " does not exist")
+        );
+
+        // Save the application letter document to the database.
+        String letterId = documentService.createDocument(file, internshipOffer.getFirmId()).getDocumentId();
+        Document letter = documentService.getDocument(letterId);
+
+        // Create and save the internship record in the database.
+        Internship internship = internshipRepository.save(
+                Internship.builder()
+                          .student(student)
+                          .internshipOffer(internshipOffer)
+                          .applicationLetter(letter)
+                          .applicationFormAcceptanceStatus(AcceptanceStatus.NotEvaluated)
+                          .build()
+        );
+
+        // Send notification to the firm
+        notificationService.createNotification(internshipOffer.getFirmId(),
+                CreateNotificationRequest
+                        .builder()
+                        .content("The student " + student.getStudentNumber() + " has sent an application letter.")
+                        .build());
+
+        // Return the Internship ID.
+        return new SendApplicationLetterResponse(internship.getId());
+    }
+}
